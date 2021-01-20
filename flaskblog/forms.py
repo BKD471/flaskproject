bff360598b83adaac5d4eda4f2fff8f633672d64
Bhaskar <bhaskar.kumardas@aot.edu.in>
2021-01-20 22:54:21 +0530

    C:/Users/User/Anaconda3/Scripts/activate

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from flask_wtf.file import FileField,FileAllowed
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
 from wtforms.validators import DataRequired,Length,Email,ValidationError,EqualTo
 
 from flaskblog.models import User
@@ -52,4 +52,8 @@ class LoginForm(FlaskForm):
     remember=BooleanField('Remember Me')
     submit=SubmitField('Log in')
 
+class PostForm(FlaskForm):
+    title=StringField('Title',validators=[DataRequired()])
+    content=TextAreaField('Content',validators=[DataRequired()])
+    submit=SubmitField('Post')
 
